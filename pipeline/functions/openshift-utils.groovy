c67d73451922bf3e29bccd31631bae3dcbdf0bac
David Sancho <dsanchor@redhat.com>
2017-09-14 11:56:10 +0200

    added some functions

--- pipeline/functions/openshift-utils.groovy
+++ pipeline/functions/openshift-utils.groovy
@@ -7,11 +7,19 @@ def createProject(project, jenkinsProject) {
    } catch ( e ) {
       echo "Check error.. but it could be that the project already exists... skkiping step"
       echo "${e}"
-      // TODO if the project was not created by jenkins sa, then, it is vey likely that its sa doesnt have admin or edit role. If it was created by jenkins, jenkins sa will have admin role
+      // TODO To be decided.. => if the project was not created by jenkins sa, then, it is vey likely that its sa doesnt have admin or edit role. If it was created by jenkins, jenkins sa will have admin role
       //openshift.policy("add-role-to-user", "edit", "system:serviceaccount:${jenkinsProject}:jenkins", "-n", project)
    }
 }
 
-def test(){ echo "It is loaded.."}
+
+def applyTemplate(project, templateFile, String... params) {
+   openshift.withProject( project ) {
+      def models = openshift.process( readFile(file:templateFile), params )
+      echo "This template has ${models.size()} objects"
+      def created = openshift.apply( models )
+     // do we want to show "created"?
+   }
+}
 
 return this
