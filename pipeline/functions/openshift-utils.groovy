edd2d219a0603d12e9ff4d6f9d2cb107c332dbbf
David Sancho <dsanchor@redhat.com>
2017-09-14 11:27:08 +0200

    added some test functions

---
+++ pipeline/functions/openshift-utils.groovy
@@ -0,0 +1,13 @@
+// openshift-utils methods
+
+def createProject(project, jenkinsProject) {
+   try {
+      // try to create the project
+      openshift.newProject( project, "--display-name", project)
+   } catch ( e ) {
+      echo "Check error.. but it could be that the project already exists... skkiping step"
+      echo "${e}"
+      // TODO if the project was not created by jenkins sa, then, it is vey likely that its sa doesnt have admin or edit role. If it was created by jenkins, jenkins sa will have admin role
+      //openshift.policy("add-role-to-user", "edit", "system:serviceaccount:${jenkinsProject}:jenkins", "-n", project)
+   }
+}
