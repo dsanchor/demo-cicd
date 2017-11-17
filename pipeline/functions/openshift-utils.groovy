24337954cc46ce4464ac77721dbf8dc46445dba5
dsanchor <dsanchor@redhat.com>
2017-11-17 16:33:13 +0100

    Update openshift-utils.groovy

--- pipeline/functions/openshift-utils.groovy
+++ pipeline/functions/openshift-utils.groovy
@@ -10,8 +10,9 @@ def createProject(project, jenkinsProject, imageStreamProject) {
       echo "${e}"
       echo "Check error.. but it could be that the project already exists... skkiping step"
    }
-   // TODO To be decided.. => if the project was not created by jenkins sa, then, it is vey likely that its sa doesnt have admin or edit role. If it was created by jenkins, jenkins sa will have admin role
-   openshift.policy("add-role-to-user", "edit", "system:serviceaccount:${jenkinsProject}:jenkins", "-n", project)
+   // TODO To be decided.. => if the project was not created by jenkins sa, 
+   //      then, it is vey likely that its sa doesnt have admin or edit role. If it was created by jenkins, jenkins sa will have admin role
+   // openshift.policy("add-role-to-user", "edit", "system:serviceaccount:${jenkinsProject}:jenkins", "-n", project)
    // if project and imageStreamProject are different, add system:image-puller role to project sa
    if (!project.equals(imageStreamProject)) {
       openshift.policy("add-role-to-group", "system:image-puller", "system:serviceaccounts:${project}", "-n",  imageStreamProject)	   
